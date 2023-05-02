e1288940f9552bd0df923ddf8fc12e1568539326
ryuto0111 <129473003+ryuto0111@users.noreply.github.com>
2023-05-02 04:55:40 +0000

    [Start] 3-5

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -18,10 +18,10 @@ class ListsController < ApplicationController
   end
 
   def edit
+    @list = List.find(params[:id])
   end
 
   private
-
   def list_params
     params.require(:list).permit(:title, :body)
   end
